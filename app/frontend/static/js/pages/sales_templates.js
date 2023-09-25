e3a3cd968b81baddbf1db6a8284c0fbdd32f1a43
fpfiste <=>
2023-09-25 20:05:31 +0200

    fix seed data ids

---
+++ app/frontend/static/js/pages/sales_templates.js
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<filetype binary="false" default_extension="" description="Javascript" name="Javascript">
+  <highlighting>
+    <options>
+      <option name="LINE_COMMENT" value="//" />
+      <option name="COMMENT_START" value="/*" />
+      <option name="COMMENT_END" value="*/" />
+      <option name="HEX_PREFIX" value="" />
+      <option name="NUM_POSTFIXES" value="" />
+      <option name="HAS_BRACES" value="true" />
+      <option name="HAS_BRACKETS" value="true" />
+      <option name="HAS_PARENS" value="true" />
+      <option name="HAS_STRING_ESCAPES" value="true" />
+    </options>
+    <keywords keywords="break;case;catch;class;const;continue;debugger;default;delete;do;else;export;extends;finally;for;function;if;import;in;instanceof;let;new;return;super;switch;this;throw;try;typeof;var;void;while;with;yield" ignore_case="false" />
+  </highlighting>
+  <extensionMap>
+    <mapping ext="js" />
+  </extensionMap>
+</filetype>
