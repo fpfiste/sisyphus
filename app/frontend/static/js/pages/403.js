c35e52ee9f11a50ba447836b431fd12a90393cb8
fpfiste <=>
2023-10-10 20:30:29 +0200

    403 page

--- app/frontend/static/js/pages/403.js
+++ app/frontend/static/js/pages/403.js
@@ -3,6 +3,7 @@ jQuery.fn.setUp = function(page_config, fields) {
     // create table instance
     let lang_cookie = Cookies.get('sisyphus_language');
 
+    $('#error_title').text(page_config['title'][lang_cookie])
     $('#error_message').text(page_config['message'][lang_cookie])
 
 
