40696946de5c052446138eb6583e2ae21e520696
fpfiste <=>
2023-11-13 12:18:24 +0100

    fix backup timestamp format

--- background_jobs/scripts/backup.sh
+++ background_jobs/scripts/backup.sh
@@ -2,6 +2,6 @@
 
 find /backup/ -mindepth 1 -mtime +90 -delete
 
-timestamp=$(date +%s)
+timestamp=$(date +%Y%m%d_%H%M%S)
 PGPASSWORD="$POSTGRES_PASSWORD" pg_dump -h $SQL_HOST -p $SQL_PORT -U $POSTGRES_USER $POSTGRES_DB > /backup/$timestamp.dump
 
