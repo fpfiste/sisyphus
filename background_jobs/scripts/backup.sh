10d760ef4e6e6faa7b7ea3d18e31f2431fd1ce73
fpfiste <=>
2023-12-28 08:27:39 +0100

    change background jobs

--- background_jobs/scripts/backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-find /backup/ -mindepth 1 -mtime +90 -delete
-
-timestamp=$(date +%Y%m%d_%H%M%S)
-PGPASSWORD="$POSTGRES_PASSWORD" pg_dump -h $SQL_HOST -p $SQL_PORT -U $POSTGRES_USER $POSTGRES_DB > /backup/$timestamp.dump
-
